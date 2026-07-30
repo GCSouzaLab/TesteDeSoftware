ccc29c2f8923ba3a04dc1a48433da3a92ab96967
Gabriel Cardoso <gabrielcardososouza3007@gmail.com>
2026-07-30 20:31:00 -0300

    Adicionando classe de teste

---
+++ src/test/java/br/com/testsoftware/CadastroChavePixTest.java
@@ -0,0 +1,22 @@
+package br.com.testsoftware;
+
+import org.junit.Test;
+
+class CadastroChavePixTest {
+
+    @Test
+    public void cadastroChavePixCPFValido(){}
+
+    @Test
+    public void cadastroChavePixCNPJInvalido(){}
+
+    @Test
+    public void tentativaCadastroEntradaMaliciosa(){}
+
+    @Test
+    public void cadastroChavePixCNPJValido(){}
+
+    @Test
+    public void cadastroChavePixCPFInvalido(){}
+
+}
