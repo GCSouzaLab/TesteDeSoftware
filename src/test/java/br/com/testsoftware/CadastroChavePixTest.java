9317b1e649bd181134df515f0d4e5649f65ab864
Gabriel Cardoso <gabrielcardososouza3007@gmail.com>
2026-07-30 21:18:24 -0300

    Adicionando testes unitarios estrategia TDD - version Red

--- src/test/java/br/com/testsoftware/CadastroChavePixTest.java
+++ src/test/java/br/com/testsoftware/CadastroChavePixTest.java
@@ -1,26 +1,53 @@
 package br.com.testsoftware;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CadastroChavePixTest {
 
     @Test
     public void cadastroChavePixCPFValido(){
-        String cpfValido = "";
+        String cpfValido = "97777857550";
 
+        ValidadorChavePIX validador = new ValidadorChavePIX(cpfValido);
+        boolean isCPFValido = validador.validarCPF();
 
+        Assert.assertTrue(isCPFValido);
     }
 
     @Test
     public void cadastroChavePixCNPJInvalido(){}
 
     @Test
-    public void tentativaCadastroEntradaMaliciosa(){}
+    public void tentativaCadastroEntradaMaliciosa(){
+        String cpfMalicioso = "'' OR '1'='1'";
+
+        ValidadorChavePIX validador = new ValidadorChavePIX(cpfMalicioso);
+        Exception exception = Assert.assertThrows(IllegalArgumentException.class, validador::validarCPF);
+
+        String mensagemEsperada = "O chave pix está inválida!";
+        String mensagemAtual = exception.getMessage();
+        Assert.assertEquals(mensagemEsperada, mensagemAtual);
+    }
 
     @Test
-    public void cadastroChavePixCNPJValido(){}
+    public void cadastroChavePixCNPJValido(){
+        String chavePix = "34.577.439/0001-01";
+        ValidadorChavePIX validador = new ValidadorChavePIX(chavePix);
+
+        Assert.assertTrue(validador.validarCNPJ());
+    }
 
     @Test
-    public void cadastroChavePixCPFInvalido(){}
+    public void cadastroChavePixCPFInvalido(){
+        String cpfInvalido = "111.111.111-11";
+
+        ValidadorChavePIX validador = new ValidadorChavePIX(cpfInvalido);
+        Exception exception = Assert.assertThrows(IllegalArgumentException.class, validador::validarCPF);
+
+        String mensagemEsperada = "O CPF está inválido! Informe um novo CPF.";
+        String mensagemAtual = exception.getMessage();
+        Assert.assertEquals(mensagemEsperada, mensagemAtual);
+    }
 
 }
