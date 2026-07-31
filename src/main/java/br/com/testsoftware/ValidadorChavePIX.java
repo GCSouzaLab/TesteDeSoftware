3a6b684f9d01c8ce9fdc3c3bc3fd4eabd6afbbce
Gabriel Cardoso <gabrielcardososouza3007@gmail.com>
2026-07-31 20:05:21 -0300

    adicionando solucao para o cpf

--- src/main/java/br/com/testsoftware/ValidadorChavePIX.java
+++ src/main/java/br/com/testsoftware/ValidadorChavePIX.java
@@ -11,10 +11,58 @@ public class ValidadorChavePIX {
     }
 
     public boolean validarCPF() {
-        if (chavePix == null) throw new IllegalArgumentException("Mensagem...");
-        return true;
-    }
+        String cpf = this.chavePix;
 
+        cpf = cpf.replace(".", "");
+        cpf = cpf.replace("-", "");
+        if (cpf.length()!=11) {
+            throw new IllegalArgumentException("O CPF está inválido! Informe um novo CPF.");
+        }
+        for (int i = 1; i < 11; i++) {
+            if (cpf.charAt(i)!= cpf.charAt(i-1)) {
+                break;
+            }else if (i==10) {
+                throw new IllegalArgumentException("O CPF está inválido! Informe um novo CPF.");
+            }
+        }
+        int soma = 0;
+        int Dv1 = 0;
+        int Dv2 = 0;
+        String Dv = cpf.substring(10);
+        for (int i = 0; i < 9; i++) {
+            soma += (10-i)*Character.getNumericValue(cpf.charAt(i));
+            System.out.println(soma);
+        }
+        Dv1 = soma%11;
+        if (Dv1 < 2) {
+            Dv1 = 0;
+        } else {
+            Dv1 = 11-Dv1;
+        }
+        System.out.println(Dv1);
+        soma=0;
+        cpf = cpf.substring(0,9);
+        String cpfDv2 =cpf + Dv1;
+        for (int i = 0; i < cpfDv2.length(); i++) {
+            soma += (11-i)*Character.getNumericValue(cpfDv2.charAt(i));
+        }
+        Dv2 = soma%11;
+        if (Dv2 < 2) {
+            Dv2 = 0;
+        } else {
+            Dv2 = 11-Dv2;
+        }
+        System.out.println(Dv2);
+        cpf += Dv1;
+        cpf += Dv2;
+        System.out.println(cpf);
+
+        if (cpf.substring(10).equals(Dv)) {
+            return true;
+        } else {
+            throw new IllegalArgumentException("O CPF está inválido! Informe um novo CPF.");
+        }
+    }
 
     public boolean validarCNPJ() {
         if (chavePix == null || !chavePix.matches(CNPJ_SEGURO_PATTERN))
